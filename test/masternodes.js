520fddc40020a05dff0ffc86660a7ddc44f4ce6c
pioncore <info@pioncoin.org>
2018-11-28 17:16:30 +0530

    rebrand to pion

--- test/masternodes.js
+++ test/masternodes.js
@@ -54,7 +54,7 @@ describe('Masternode', function() {
 		}];
 		var node = {
 			services: {
-        dashd: {
+        piond: {
 					getMNList: sinon.stub().callsArgWith(0, null, MNList)
 				}
 			}
